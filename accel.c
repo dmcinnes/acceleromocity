7325e458e21a7a85ca5b0e85edcdc9681e5b59ec
Doug McInnes <doug@dougmcinnes.com>
2013-09-17 22:49:53 -0700

    testing

--- accel.c
+++ accel.c
@@ -22,7 +22,9 @@
  The MMA8452 has built in pull-up resistors for I2C so you do not need additional pull-ups.
  */
 
-#include <Arduino.h>
+#define F_CPU 8000000UL // 8 MHz
+
+#include <util/delay.h>
 #include <i2cmaster.h>
 
 // The SparkFun breakout board defaults to 1, set to 0 if SA0 jumper on the bottom of the board is set
@@ -46,18 +48,18 @@ static float ledsY[3] = { -0.5, -0.5, 1.0 };
 static unsigned int ledCount = 3;
 
 
-// Read bytesToRead sequentially, starting at addressToRead into the dest byte array
-void readRegisters(byte addressToRead, int bytesToRead, byte * dest)
+// Read uint8_tsToRead sequentially, starting at addressToRead into the dest uint8_t array
+void readRegisters(uint8_t addressToRead, int uint8_tsToRead, uint8_t * dest)
 {
   /*TinyWireM.beginTransmission(MMA8452_ADDRESS);*/
   /*TinyWireM.send(addressToRead);*/
   /*TinyWireM.endTransmission(); //endTransmission but keep the connection active*/
 
-  /*TinyWireM.requestFrom(MMA8452_ADDRESS, bytesToRead); //Ask for bytes, once done, bus is released by default*/
+  /*TinyWireM.requestFrom(MMA8452_ADDRESS, uint8_tsToRead); //Ask for uint8_ts, once done, bus is released by default*/
 
-  /*while(TinyWireM.available() < bytesToRead); //Hang out until we get the # of bytes we expect*/
+  /*while(TinyWireM.available() < uint8_tsToRead); //Hang out until we get the # of uint8_ts we expect*/
 
-  /*for(int x = 0 ; x < bytesToRead ; x++)*/
+  /*for(int x = 0 ; x < uint8_tsToRead ; x++)*/
   /*  dest[x] = TinyWireM.receive();*/
 
   i2c_start_wait(MMA8452_ADDRESS+I2C_WRITE);     // set device address and write mode
@@ -65,34 +67,37 @@ void readRegisters(byte addressToRead, int bytesToRead, byte * dest)
   i2c_write(addressToRead);                        // write address
   i2c_rep_start(MMA8452_ADDRESS+I2C_READ);       // set device address and read mode
 
-  for(int x = 0 ; x < bytesToRead-1 ; x++)
-    dest[x] = i2c_read(1);                    // read one byte and request more
+  for(int x = 0 ; x < uint8_tsToRead-1 ; x++)
+    dest[x] = i2c_read(1);                    // read one uint8_t and request more
 
-  dest[bytesToRead-1] = i2c_read(0);                    // read one byte and stop
+  dest[uint8_tsToRead-1] = i2c_read(0);                    // read one uint8_t and stop
 }
 
-// Read a single byte from addressToRead and return it as a byte
-byte readRegister(byte addressToRead)
+// Read a single uint8_t from addressToRead and return it as a uint8_t
+uint8_t readRegister(uint8_t addressToRead)
 {
   /*TinyWireM.beginTransmission(MMA8452_ADDRESS);*/
   /*TinyWireM.send(addressToRead);*/
   /*TinyWireM.endTransmission(); //endTransmission but keep the connection active*/
 
-  /*TinyWireM.requestFrom(MMA8452_ADDRESS, 1); //Ask for 1 byte, once done, bus is released by default*/
+  /*TinyWireM.requestFrom(MMA8452_ADDRESS, 1); //Ask for 1 uint8_t, once done, bus is released by default*/
 
   /*while(!TinyWireM.available()) ; //Wait for the data to come back*/
-  /*return TinyWireM.receive(); //Return this one byte*/
+  /*return TinyWireM.receive(); //Return this one uint8_t*/
 
   i2c_start_wait(MMA8452_ADDRESS+I2C_WRITE);     // set device address and write mode
 
   i2c_write(addressToRead);                        // write address
   i2c_rep_start(MMA8452_ADDRESS+I2C_READ);       // set device address and read mode
 
-  return i2c_readNak();                    // read one byte from EEPROM
+  uint8_t value = i2c_readNak();                    // read one uint8_t from EEPROM
+  i2c_stop();
+
+  return value;
 }
 
-// Writes a single byte (dataToWrite) into addressToWrite
-void writeRegister(byte addressToWrite, byte dataToWrite)
+// Writes a single uint8_t (dataToWrite) into addressToWrite
+void writeRegister(uint8_t addressToWrite, uint8_t dataToWrite)
 {
   /*TinyWireM.beginTransmission(MMA8452_ADDRESS);*/
   /*TinyWireM.send(addressToWrite);*/
@@ -106,23 +111,23 @@ void writeRegister(byte addressToWrite, byte dataToWrite)
 }
 
 // Sets the MMA8452 to standby mode. It must be in standby to change most register settings
-void MMA8452Standby()
+void MMA8452Standby(void)
 {
-  byte c = readRegister(CTRL_REG1);
+  uint8_t c = readRegister(CTRL_REG1);
   writeRegister(CTRL_REG1, c & ~(0x01)); //Clear the active bit to go into standby
 }
 
 // Sets the MMA8452 to active mode. Needs to be in this mode to output data
-void MMA8452Active()
+void MMA8452Active(void)
 {
-  byte c = readRegister(CTRL_REG1);
+  uint8_t c = readRegister(CTRL_REG1);
   writeRegister(CTRL_REG1, c | 0x01); //Set the active bit to begin detection
 }
 
 
 void readAccelData(int *destination)
 {
-  byte rawData[6];  // x/y/z accel register data stored here
+  uint8_t rawData[6];  // x/y/z accel register data stored here
 
   readRegisters(OUT_X_MSB, 6, rawData);  // Read the six raw data registers into data array
 
@@ -146,28 +151,43 @@ void readAccelData(int *destination)
 // Initialize the MMA8452 registers
 // See the many application notes for more info on setting all of these registers:
 // http://www.freescale.com/webapp/sps/site/prod_summary.jsp?code=MMA8452Q
-void initMMA8452()
+void initMMA8452(void)
 {
-  byte c = readRegister(WHO_AM_I);  // Read WHO_AM_I register
+  _delay_ms(500); // give MMA8452 a chance to boot
+
+  i2c_init();
+
+  uint8_t ret = i2c_start(MMA8452_ADDRESS+I2C_WRITE);       // set device address and write mode
+
+  if ( ret ) {
+      /* failed to issue start condition, possibly no device found */
+      i2c_stop();
+    while(1) {
+      PORTA ^= 1 << PA0;
+      _delay_ms(50);
+    } // Loop forever if communication doesn't happen
+  }
+
+  uint8_t c = readRegister(WHO_AM_I);  // Read WHO_AM_I register
   if (c == 0x2A) // WHO_AM_I should always be 0x2A
   {
     /*Serial.println("MMA8452Q is online...");*/
-    analogWrite(0, 0);
+    PORTA |= 1 << PA0;
   }
   else
   {
     /*Serial.print("Could not connect to MMA8452Q: 0x");*/
     /*Serial.println(c, HEX);*/
-    unsigned int i = 0;
     while(1) {
-      analogWrite(0, (i++/20) % 255);
+      PORTA ^= 1 << PA0;
+      _delay_ms(500);
     } // Loop forever if communication doesn't happen
   }
 
   MMA8452Standby();  // Must be in standby to change registers
 
   // Set up the full scale range to 2, 4, or 8g.
-  byte fsr = GSCALE;
+  uint8_t fsr = GSCALE;
   if(fsr > 8) fsr = 8; //Easy error check
   fsr >>= 2; // Neat trick, see page 22. 00 = 2G, 01 = 4A, 10 = 8G
   writeRegister(XYZ_DATA_CFG, fsr);
@@ -177,24 +197,26 @@ void initMMA8452()
   MMA8452Active();  // Set to active to start reading
 }
 
-void setup()
+unsigned long millis(void) {
+  return 0;
+}
+
+void setup(void)
 {
   /*Serial.begin(57600);*/
   /*Serial.println("MMA8452 Basic Example");*/
   lastTime = millis();
 
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-
-  analogWrite(0, 255);
-
-  i2c_init();
+  /* pinMode(0, OUTPUT); */
+  /* pinMode(1, OUTPUT); */
+  /* pinMode(2, OUTPUT); */
+  // set pins 0, 1 and 2 to output
+  DDRA = (1<<DDA0) | (1<<DDA1) | (1<<DDA2);
 
   initMMA8452(); //Test and intialize the MMA8452
 }
 
-void loop()
+void loop(void)
 {
   int i, output;
   float dot;
@@ -222,9 +244,13 @@ void loop()
       dot = currentAcc[0] * ledsX[i] + currentAcc[1] * ledsY[i];
 
       // invert dot so the up pointing led is lit
-      output = (int) (255 * constrain(-dot, 0, 1));
+      /* output = (int) (255 * constrain(-dot, 0, 1)); */
 
-      analogWrite(i, output);
+      if (-dot > 0.5) {
+        PORTA |= (1<<PA0);
+      } else {
+        PORTA &= ~(0<<PA0); // clear PA0
+      }
 
       /*Serial.print(accelG[i], 2);  // Print g values*/
       /*Serial.print("/");*/
@@ -238,10 +264,9 @@ void loop()
   }
 }
 
-int main() {
-  init();
+int main(void) {
   setup();
   while(1) {
-    loop();
+    /* loop(); */
   }
 }
